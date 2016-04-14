88a7646822b4437d950ab11a46cdf2b8883a0d7d
Nick Evans <nick@imin.co>
2016-04-14 23:00:22 +0100

    Added CI

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# squash messages
+git config --global push.default matching
+
+# clear and re-create the out directory
+rm -rf out || exit 0;
+mkdir out;
+
+# go to the out directory 
+cd out
+
+# get existing gh-pages
+git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}"
+
+cd openactive.github.io
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "travis@openactive.org"
+
+# unzip
+unzip -o openactive-demonstrator.webflow.zip
+
+# (hack for now!) add a date to ensure there's always something to push!
+date > timestamp.txt
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
+
